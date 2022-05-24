5ec2455f704cb3b372e83aaf6c6c25c7ed6f8d7e
Per Stensland <pertan4711@hotmail.com>
2022-05-24 09:13:41 +0200

    Fix för nyinstallation av java och javafx

--- src/Othello.java
+++ src/Othello.java
@@ -1,6 +1,6 @@
 import java.util.Optional;
 
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -23,6 +23,13 @@ import javafx.stage.Stage;
 // https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/
 // https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-4-alpha-beta-pruning/
 //==============================================
+
+// Setup VSCode - Java/JavaFX
+// Installera JAVA och sätt JAVA_HOME
+// Installera JavaFX från openjfx.io. Sätt PATH_TO_FX till katalogen där du installerade fx och lägg till 'lib' i sökvägen
+// Ex: PATH_TO_FX  C:\tools\javafx-sdk-18.0.1\lib
+// Modifiera launch.json  "vmArgs": "--module-path \"${env:PATH_TO_FX}\" --add-modules javafx.controls,javafx.fxml",
+// Möjligt behöver även settings.json modifieras så att den innehåller sökvägen till alla fx-moduler
 
 // TODO Vad är kriteriet för gameover? (funkar inte riktigt)
 // - Ingen kan lägga ett giltigt move
